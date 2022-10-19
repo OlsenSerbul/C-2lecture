1cd619455d59b30c4e619189849a79b6295f1f94
Olga Serbul <Olsen.zika@mail.ru>
2022-10-19 11:47:21 +0300

    Task1_практика_на_семинаре

---
+++ Практика_на_семинаре/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+int number = new Random().Next(10, 100);
+int a1 = number / 10;
+int a2 = number % 10;
+Console.WriteLine(number);
+
+if (a1 > a2) Console.WriteLine($"Наибольшая цифра в числе: {a1}");
+
+if (a1 < a2) Console.WriteLine($"Наибольшая цифра в числе: {a2}");
+
+if (a1 == a2) Console.WriteLine($"Цифры в числе равны ");
+
