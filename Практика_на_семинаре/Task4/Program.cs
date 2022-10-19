5013518b35ac4a1c0a3278c19db7fcf655b8e787
Olga Serbul <Olsen.zika@mail.ru>
2022-10-19 17:26:59 +0300

    Task4_практика_на_семинаре

---
+++ Практика_на_семинаре/Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+Console.WriteLine("Введите число А:  ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int sqrA = numberA * numberA;
+int sqrB = numberB * numberB;
+
+if (sqrA == numberB || sqrB == numberA) Console.WriteLine(" Да ");
+else
+Console.WriteLine(" Нет ");
