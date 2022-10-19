5b478e0291e5d68f85d211075ac8942253f4b479
Olga Serbul <Olsen.zika@mail.ru>
2022-10-19 17:13:41 +0300

    Task02_домашняя_работа

---
+++ Домашняя_работа/Task02/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+// запускаем генератор случайных чисел
+Random rnd = new Random() ;
+int number = rnd.Next();
+Console.WriteLine(number);
+int Number = number;
+
+// запускаем цикл с проверкой есть ли в числе третья цифра
+
+while (number / 100 > 0 )
+{
+    if (number < 1000 && number > 100)
+    {
+        number = number % 10;
+        Console.WriteLine($"В числе {Number} третья цифра {number}" );
+    }
+    else
+    {
+        number = number / 10;
+    }
+ 
+}
+ Console.WriteLine($"В числе {Number} третьей цифры нет" );
+
+
+
+ number = number % 10;
+     Console.WriteLine($"В числе {Number} третья цифра {number});
+
+     if 
